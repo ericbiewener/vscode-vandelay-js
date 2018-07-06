17c4ec75dc4aa588a8b024fcefb2772029e708fa
Eric Biewener <eric@talentpair.com>
2018-07-05 21:07:21 -0700

    Got regex done

--- src/regex.js
+++ src/regex.js
@@ -1,37 +1,47 @@
-// Imports
-const importRegex = {
-  singleLine: /^from +?(.+) +?import +?([^(\n\\]+\n)/gm,
-  multiline: /^from +?(.+) +?import +\(([\S\s]*?)\)/gm,
-}
-// TODO: can i get python one into a single regex?
-^import +?(?:(\w+)[, ])? *(?:({[^]*?}) +)?from +["|'](.*)["|']
+import _ from 'lodash'
 
-function parseImportsWithRegex(text, regex, replacer, imports = []) {
-  let match
-  while ((match = regex.exec(text))) {
-    const results = {
-      path: match[1],
-      start: match.index,
-      end: match.index + match[0].length,
-    }
-    // entirePackage regex does not provide a second matching group
-    if (match[2]) results.imports = match[2].replace(replacer, '').split(',')
-    imports.push(results)
-  }
-  return imports
-}
+/**
+ * Regex must end with `.*` after last capturing group to ensure that we capture the full line.
+ * This is necessary so that the `end` property in the results is the correct character.
+ *
+ * Matching groups:
+ *    1. default import
+ *    2. named/type imports
+ *    3. path
+ */
+const importRegex = /^import +?(?:(\w+)[, ])? *(?:{([^]*?)} +)?from +["|'](.*)["|'].*/gm
 
 function parseImports(text) {
-  // Mutate imports
-  const imports = parseImportsWithRegex(text, importRegex.entirePackage)
-  parseImportsWithRegex(text, importRegex.singleLine, /\s/g, imports)
-  return parseImportsWithRegex(text, importRegex.multiline, /[\s()]/g, imports)
+  const imports = []
+  let match
+  while ((match = importRegex.exec(text))) {
+    const results = {
+      path: match[3],
+      start: match.index,
+      end: match.index + match[0].length,
+      default: match[1]
+    }
+    if (match[2]) {
+      const namedAndTypes = match[2]
+        .replace(/{}]/g, '')
+        .split(',')
+        .map(i => i.trim())
+
+      const groups = _.partition(namedAndTypes, i => i.startsWith('type '))
+      results.typesnamed = groups[0]
+      results.named = groups[1]
+    }
+    imports.push(results)
+  }
+
+  importRegex.lastIndex = 0
+  return imports
 }
 
 // Comments
 const comments = /^(?:[ \t]*\/\/|[ \t]*\/\*[^]*?\*\/)/gm
 
-// #TODO: make part of vandelay-core, that accets args of `text, singleLineRegex, multilineRegex`
+// #TODO: make part of vandelay-core, that accepts args of `text, singleLineRegex, multilineRegex`
 function getLastInitialComment(text) {
   // Iterates over comment line matches. If one doesn't begin where the previous one left off, this means
   // a non comment line came between them.
